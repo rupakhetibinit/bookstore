60711afd3ce6c9e214d99d462591aaa94f40e976
Binit Rupakheti <cruiserponder@protonmail.com>
2022-12-19 19:26:55 +0545

    Admin side ui and api for login

--- pages/admin/login.tsx
+++ pages/admin/login.tsx
@@ -1,9 +1,72 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 type Props = {};
 
 const login = (props: Props) => {
-	return <div>Login</div>;
+	const router = useRouter();
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+		const formValues = e.target as any as Record<
+			'email' | 'password',
+			{
+				value: string;
+			}
+		>;
+		const email = formValues.email.value;
+		const password = formValues.password.value;
+		const response = await fetch('/api/admin/login', {
+			method: 'POST',
+			body: JSON.stringify({ email, password }),
+		});
+		console.log(response);
+		if (response.redirected) return router.push(response.url);
+	};
+	return (
+		<div className='w-screen flex items-center justify-center h-screen bg-white'>
+			<div className='h-96 w-72'>
+				<form
+					method='post'
+					className='p-4 bg-white space-y-8'
+					onSubmit={handleSubmit}
+					action='/api/admin/login'>
+					<div className='relative'>
+						<input
+							id='email'
+							name='email'
+							type='email'
+							className='peer w-full h-10 border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent'
+							placeholder='Email Address'
+						/>
+						<label
+							className='absolute -top-3.5 left-0 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm hover:cursor-text'
+							htmlFor='email'>
+							Email Address
+						</label>
+					</div>
+					<div className='relative'>
+						<input
+							id='password'
+							name='password'
+							type='password'
+							className='peer w-full h-10 border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent'
+							placeholder='Password'
+						/>
+						<label
+							className='absolute -top-3.5 left-0 to-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm hover:cursor-text'
+							htmlFor='password'>
+							Password
+						</label>
+					</div>
+					<button
+						type='submit'
+						className='bg-rose-500 rounded-md text-white px-8 py-2 mx-auto w-full'>
+						Sign In
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default login;
---
+++ pages/api/admin/login.ts
@@ -0,0 +1,44 @@
+// pages/api/login.ts
+import { AuthRequest } from '@lucia-auth/nextjs';
+import { Role } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { auth } from '../../../lib/lucia';
+import { prisma } from '../../../lib/prisma';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method !== 'POST')
+		return res.status(404).json({ error: 'Not found' });
+	const { email, password } = JSON.parse(req.body);
+	if (
+		!email ||
+		!password ||
+		typeof email !== 'string' ||
+		typeof password !== 'string'
+	)
+		return res.status(400).json({});
+	try {
+		const authRequest = new AuthRequest(auth, req, res);
+		const user = await auth.authenticateUser('email', email, password);
+		const session = await auth.createSession(user.userId);
+		authRequest.setSession(session); // set cookie
+		const adminOrNot = await prisma.user.findUnique({
+			where: {
+				email: email,
+			},
+			select: {
+				role: true,
+			},
+		});
+		console.log(adminOrNot);
+		if (adminOrNot?.role !== Role.ADMIN) {
+			return res.redirect(302, '/forbidden');
+		} else {
+			return res.redirect(302, '/admin/dashboard'); // redirect to profile page
+		}
+	} catch {
+		// invalid
+		return res.status(200).json({
+			error: 'Incorrect username or password',
+		});
+	}
+};
