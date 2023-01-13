0b00bfc324bf5ffddd8a9c9668bf72287396ec5f
Binit Rupakheti <cruiserponder@protonmail.com>
2023-01-13 22:48:13 +0545

    refactored toggle menu

--- components/ToggleButton.tsx
+++ components/ToggleButton.tsx
@@ -6,7 +6,21 @@ interface ToggleProps {
 export const Toggle = ({ toggle, menuOpen }: ToggleProps) => {
 	return (
 		<button onClick={toggle}>
-			{!menuOpen && (
+			{menuOpen ? (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					strokeWidth={1.5}
+					stroke='currentColor'
+					className='w-6 h-6 pointer-events-none'>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						d='M6 18L18 6M6 6l12 12'
+					/>
+				</svg>
+			) : (
 				<svg
 					onClick={toggle}
 					xmlns='http://www.w3.org/2000/svg'
@@ -19,21 +33,6 @@ export const Toggle = ({ toggle, menuOpen }: ToggleProps) => {
 						strokeLinecap='round'
 						strokeLinejoin='round'
 						d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-					/>
-				</svg>
-			)}
-			{menuOpen && (
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					fill='none'
-					viewBox='0 0 24 24'
-					strokeWidth={1.5}
-					stroke='currentColor'
-					className='w-6 h-6 pointer-events-none'>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						d='M6 18L18 6M6 6l12 12'
 					/>
 				</svg>
 			)}
